a02f8994864d5b7d917d12a4f8c0b3611f27ce80
FavouriteDragon <ninjadragon008@icloud.com>
2019-09-18 11:00:35 -0700

    Workign on test classes, got some util methods
    Added a new util class with methods for initializing teleop motors and handling gamepad controls. Also reorganised the skystone code to make it more readable.

--- src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/Autonomous/AutonomousTest.java
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/Autonomous/AutonomousTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.SkyStone;
+package org.firstinspires.ftc.teamcode.SkyStone.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -6,7 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "Autonomous", group = "Linear Opmode")
-public class AutonomousTest {
+import java.lang.annotation.Annotation;
 
+@Autonomous(name = "Autonomous", group = "Linear Opmode")
+public class AutonomousTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
