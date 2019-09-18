79364adb72d769e683fa97d16e04851105c22bfb
Edward <edwardyu303210@gmail.com>
2019-09-18 11:17:45 -0700

    Changing file paths for better organization
    Fixed naming conventions for folders; now lowercase.

--- src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/SkyStoneUtils.java
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/SkyStoneUtils.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.SkyStone;
+package org.firstinspires.ftc.teamcode.skystone;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
