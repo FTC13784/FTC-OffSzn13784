b6a6c0fefe6bdf8550e7e09ab897854cdb64eb2d
Joey <jmock1881@gmail.com>
2019-10-15 10:10:46 -0700

    Autonomous and claw code

--- src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/autonomous/AutonomousTest.java
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/autonomous/AutonomousTest.java
@@ -1,18 +1,32 @@
-package org.firstinspires.ftc.teamcode.SkyStone.Autonomous;
+package org.firstinspires.ftc.teamcode.skystone.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import java.lang.annotation.Annotation;
-
-@Autonomous(name = "Autonomous", group = "Linear Opmode")
+@Autonomous(name = "AutonomousTest", group = "Autonomous")
 public class AutonomousTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    EncoderFun ourBot;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        ourBot = new EncoderFun(this);
 
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        telemetry.addData("Status", "Start received");
+        telemetry.update();
+
+        ourBot.driveTicks(50000, 1);
+        ourBot.driveTime(10,1);
+
+        telemetry.addData("Statues", "Done");
+        telemetry.update();
     }
 }
---
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/autonomous/EncoderFun.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.skystone.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class autoDiag {
+    public void diagLeft (int degrees, double speed) {
+
+    }
+} //AutoDiag
