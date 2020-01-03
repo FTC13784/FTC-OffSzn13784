ac84d759a3d28ea597ae5e2cbb2f98ba3d861fd9
FavouriteDragon <ninjadragon008@icloud.com>
2020-01-03 13:35:32 -0800

    autonomousfoundation

---
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/autonomous/AutonomousFoundation.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.skystone.autonomous
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Drives forward, grabs foundation, goes back, and parks ON THE LINE
+ * for points.
+ *
+ * @author FavouriteDragon
+ */
+@Autonomous(name = "AutonomousFoundation", group = "Autonomous")
+public class AutonomousFoundation extends LinearOpMode {
+
+    private ElapsedTime runTime = new ElapsedTime();
+
+    EncoderFunLight bot;
+    long start, now, duration;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
