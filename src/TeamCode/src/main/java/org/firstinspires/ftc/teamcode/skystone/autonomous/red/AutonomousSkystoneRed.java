905b6d8bf65665b6423b391b488bd17ef0e15c30
Robert <robertpmenezes@gmail.com>
2020-01-31 10:28:58 -0800

    quick javadoc for a general skystone autonomous class

---
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skystone/autonomous/red/AutonomousSkystoneRed.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.skystone.autonomous.red;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//@Autonomous(group = "Autonomous", name = "SkystoneGeneralRed")
+public class AutonomousSkystoneRed extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Use .properties files to dictate what to do in the program
+        /**
+         * Options:
+         * @coloured True or false boolean for whether to go after the special skystones or whether
+         * to just get the first couple.
+         * @amount The amount of skystones to get. Coloured skystones auto-cap at two
+         * @parkWall Boolean for whether to park at the wall (bottom) or at the bridhe (top)
+         */
+    }
+}
