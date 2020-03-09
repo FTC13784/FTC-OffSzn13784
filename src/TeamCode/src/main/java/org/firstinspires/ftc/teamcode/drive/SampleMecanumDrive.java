d4932223fd9884df230dc8960a7dd3fbddc6c509
FavouriteDragon <ninjadragon008@icloud.com>
2020-03-09 11:24:43 -0700

    commenting out unnecessary code
    We don't have a rev hub yet

--- src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ src/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -100,9 +100,9 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
 
-        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
-            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
-        }
+      //  for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+        //    module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+      //  }
 
         // TODO: adjust the names of the following hardware devices to match your configuration
         imu = hardwareMap.get(BNO055IMU.class, "imu");
