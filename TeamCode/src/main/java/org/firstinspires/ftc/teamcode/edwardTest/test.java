45adba45a961bb1e7a50b9b19af5e2d3fa84102e
Edward <edwardyu303210@gmail.com>
2020-10-27 10:29:14 -0700

    Testing garbage for opmode
    zzzzzzzzzzzzzzzzzzzzzz

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/edwardTest/test.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.edwardTest;
+
+// import packages
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+// TeleOp declaration
+// @TeleOp(name = "Test Program", group = "test")
+
+// disable telemetry
+// @Disabled
+
+public class test extends OpMode {
+    // declare OpMode members
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    // run OpMode
+    @Override
+    public void runOpMode() {
+
+        // initialization telemetry
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+
+        // initialization finished
+        telemetry.addData("Status", "Initialization finished");
+        telemetry.update();
+
+
+        // initialization of hardware variables - REMEMBER TO CHANGE CONFIGURATION ON PHONES AS WELL
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "lf");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rf");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "lb");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rb");
+
+
+        runtime.reset();
+
+
+        @Override
+        public void runOpMode() throws InterruptedException {
+        
+        }
+    }
+}
