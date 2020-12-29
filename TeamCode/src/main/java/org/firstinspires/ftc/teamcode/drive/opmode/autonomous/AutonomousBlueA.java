0f79c98dd0010213d97c842f10d61587a60a3876
John <ninjadragon008@icloud.com>
2020-12-29 14:37:30 -0800

    example class for nerds

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/AutonomousBlueA.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autonomous;
+
+import com.acmerobotics.roadrunner.drive.Drive;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "blueA", group = "autonomous")
+public class AutonomousBlueA extends MasterAutonomous {
+
+    @Override
+    public DriveConstants.Side getSide() {
+        return DriveConstants.Side.BLUE;
+    }
+
+    @Override
+    public DriveConstants.GoalType getGoal() {
+        return DriveConstants.GoalType.A;
+    }
+
+    @Override
+    public DriveConstants.ActionType getAction() {
+        return DriveConstants.ActionType.WOBBLE;
+    }
+
+    @Override
+    public Drive getDrive() {
+        return new SampleMecanumDrive(hardwareMap);
+    }
+
+    @Override
+    public AutonomousDriver.DriveType getDriveType() {
+        return AutonomousDriver.DriveType.MECANUM;
+    }
+
+    @Override
+    public OpMode getMode() {
+        return this;
+    }
+}
